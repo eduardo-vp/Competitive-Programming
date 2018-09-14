f46b23f03a92f17e82ce48012e144026b12189af
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2018-09-13 19:54:46 -0500

    Create KMP.cpp

---
+++ Estructuras y algoritmos/KMP.cpp
@@ -0,0 +1,11 @@
+
+
+void kmp(string &cad){
+	p[0] = 0;
+	for(int i = 1; i < m; ++i){
+		p[i] = p[i-1];
+		while(p[i] > 0 and cad[p[i]] != cad[i])
+			p[i] = p[p[i]-1];
+		if(cad[p[i]] == cad[i]) p[i]++;
+	}
+}
