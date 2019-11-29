e5ab22aa14b24fe8edafc67ba823f98924a14b24
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2019-11-29 15:42:47 -0500

    Create LazySegmentTree.cpp

---
+++ Estructuras y algoritmos/LazySegmentTree.cpp
@@ -0,0 +1,35 @@
+
+struct node{
+	int mx,mn,sum;
+	node(){}
+	node(int mx, int mn, int sum):mx(mx),mn(mn),sum(sum){}
+	node merge(node x){
+		node ans;
+		ans.sum=sum+x.sum;
+		ans.mx=max(mx,sum+x.mx);
+		ans.mn=min(mn,sum+x.mn);
+		return ans;
+	}
+};
+ 
+node NEUT(0,1e9,-1e9);
+
+struct STree { // segment tree for min over integers
+	vector<node> st;int n;
+	STree(int n): st(4*n+5,node(0,0,0)), n(n) {}
+	void upd(int k, int s, int e, int p, int v){
+		if(s+1==e){st[k]=node(v,v,v);return;}
+		int m=(s+e)/2;
+		if(p<m)upd(2*k,s,m,p,v);
+		else upd(2*k+1,m,e,p,v);
+		st[k]=st[2*k].merge(st[2*k+1]);
+	}
+	node query(int k, int s, int e, int a, int b){
+		if(s>=b||e<=a)return NEUT;
+		if(s>=a&&e<=b)return st[k];
+		int m=(s+e)/2;
+		return query(2*k,s,m,a,b).merge(query(2*k+1,m,e,a,b));
+	}
+	void upd(int p, int v){upd(1,0,n,p,v);}
+	node query(int a, int b){return query(1,0,n,a,b);}
+}; // usage: STree rmq(n);rmq.init(x);rmq.upd(i,v);rmq.query(s,e);
