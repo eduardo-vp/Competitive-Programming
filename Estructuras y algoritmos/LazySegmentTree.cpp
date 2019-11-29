5abb8755f3ea135eb9234fc6ab113740c15870d8
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2019-11-29 16:03:58 -0500

    Update LazySegmentTree.cpp

--- Estructuras y algoritmos/LazySegmentTree.cpp
+++ Estructuras y algoritmos/LazySegmentTree.cpp
@@ -1,37 +1,44 @@
 
 // Gracias MarcosK
 
-struct node{
-	int mx,mn,sum;
-	node(){}
-	node(int mx, int mn, int sum):mx(mx),mn(mn),sum(sum){}
-	node merge(node x){
-		node ans;
-		ans.sum=sum+x.sum;
-		ans.mx=max(mx,sum+x.mx);
-		ans.mn=min(mn,sum+x.mn);
-		return ans;
-	}
-};
- 
-node NEUT(0,1e9,-1e9);
-
-struct STree { // segment tree for min over integers
-	vector<node> st;int n;
-	STree(int n): st(4*n+5,node(0,0,0)), n(n) {}
-	void upd(int k, int s, int e, int p, int v){
-		if(s+1==e){st[k]=node(v,v,v);return;}
+struct STree{ // example: range sum with range addition
+	vector<int> st,lazy;int n;
+	STree1(int n): st(4*n+5,0), lazy(4*n+5,0), n(n) {}
+	void init(int k, int s, int e, int *a){
+		lazy[k]=0;  // lazy neutral element
+		if(s+1==e){st[k]=a[s];return;}
 		int m=(s+e)/2;
-		if(p<m)upd(2*k,s,m,p,v);
-		else upd(2*k+1,m,e,p,v);
-		st[k]=st[2*k].merge(st[2*k+1]);
+		init(2*k,s,m,a);init(2*k+1,m,e,a);
+		st[k]=max(st[2*k],st[2*k+1]); // operation
 	}
-	node query(int k, int s, int e, int a, int b){
-		if(s>=b||e<=a)return NEUT;
+	void push(int k, int s, int e){
+		if(!lazy[k])return; // if neutral, nothing to do
+		st[k]+=lazy[k]; // update st according to lazy
+		if(s+1<e){ // propagate to children
+			lazy[2*k]+=lazy[k];
+			lazy[2*k+1]+=lazy[k];
+		}
+		lazy[k]=0; // clear node lazy
+	}
+	void upd(int k, int s, int e, int a, int b, int v){
+		push(k,s,e);
+		if(s>=b||e<=a)return;
+		if(s>=a&&e<=b){
+			lazy[k]+=v; // accumulate lazy
+			push(k,s,e);return;
+		}
+		int m=(s+e)/2;
+		upd(2*k,s,m,a,b,v);upd(2*k+1,m,e,a,b,v);
+		st[k]=max(st[2*k],st[2*k+1]); // operation
+	}
+	int query(int k, int s, int e, int a, int b){
+		if(s>=b||e<=a)return -1e9; // operation neutral
+		push(k,s,e);
 		if(s>=a&&e<=b)return st[k];
 		int m=(s+e)/2;
-		return query(2*k,s,m,a,b).merge(query(2*k+1,m,e,a,b));
+		return max(query(2*k,s,m,a,b),query(2*k+1,m,e,a,b)); // operation
 	}
-	void upd(int p, int v){upd(1,0,n,p,v);}
-	node query(int a, int b){return query(1,0,n,a,b);}
-}; // usage: STree rmq(n);rmq.init(x);rmq.upd(i,v);rmq.query(s,e);
+	void init(int *a){init(1,0,n,a);}
+	void upd(int a, int b, int v){upd(1,0,n,a,b,v);}
+	int query(int a, int b){return query(1,0,n,a,b);}
+}; // usage: STree rmq(n);rmq.init(x);rmq.upd(s,e,v);rmq.query(s,e);
