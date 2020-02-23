ce89de403c7b6be05afd8117b483af063ea8a4eb
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-02-23 16:54:37 -0500

    Create Random.cpp

---
+++ Estructuras y algoritmos/Random.cpp
@@ -0,0 +1,33 @@
+// dile a la jardinera que traigo flores
+// corner cases // int vs ll // cin vs scanf // clear structures // statement // doublesz
+#include <bits/stdc++.h>
+#define pb push_back
+#define sz(x) int(x.size())
+#define fill(x,v) memset(x,v,sizeof(x))
+#define REP(i,a,b) for(int i = int(a); i < int(b); ++i)
+#define trace(x) cout << #x << " = " << x << endl
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> ii;
+
+int main(){
+
+	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+	// for 64 bit
+	//mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+	cout << rng() << endl;
+	
+	vector<int> perm(10);
+	iota(perm.begin(),perm.end(),0);
+	shuffle(perm.begin(),perm.end(),rng);
+	for(int x : perm) cout << x << " "; cout << endl;
+	
+	 
+	// measure time
+	while( (clock()<=2.7*CLOCKS_PER_SEC) ){
+		// do sth
+	}
+	
+	return 0;
+}
