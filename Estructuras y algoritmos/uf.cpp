2b1f11fb22c7713959ab456071ce78666c3f2a03
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-02-27 19:13:53 -0500

    Update uf.cpp

--- Estructuras y algoritmos/uf.cpp
+++ Estructuras y algoritmos/uf.cpp
@@ -1,25 +1,6 @@
-class UnionFind {
-private: vi p,rank;
-public:
-	UnionFind(int N){ 
-		rank.assign(N,0);
-		p.assign(N,0); 
-		for(int i = 0; i < N; i++) p[i] = i;
-	}
-	
-	int findSet(int i){ 
-		return (p[i] == i) ? i : (p[i] = findSet(p[i]));
-	}
-	bool isSameSet(int i, int j){
-		return findSet(i) == findSet(j);
-	}
-	void unionSet(int i, int j){
-		int x = findSet(i), y = findSet(j);
-		if(rank[x] > rank[y])
-			p[y] = x;
-		else{
-			p[x] = y;
-			if(rank[x] == rank[y]) rank[y]++;
-		}
-	}
-}
+struct UF{
+	int p[N];
+	UF(){ REP(i,0,N) p[i] = i; }
+	int find(int i){ return (p[i] == i) ? i : (p[i] = find(p[i])); }
+	void join(int i, int j){ p[find(i)] = find(j); }
+};
