894c32afa0735fc05beb72e9acaf3c6e3b883b6c
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-03-07 13:11:14 -0500

    Update Hash.cpp

--- Estructuras y algoritmos/Hash.cpp
+++ Estructuras y algoritmos/Hash.cpp
@@ -1,3 +1,35 @@
+/*
+https://codeforces.com/blog/entry/74148?#comment-586706
+I highly dissuade hashing (multi)sets modulo a prime. Instead: assign random 64bit integer for each element, 
+and use xor/sum of all elements as hash of the whole set (xor if we are guaranteed there are no repetitions, 
+or we do want repetitions to cancel out, sum if we're hashing a multiset). To get those values of each element
+I use following function:
+
+ull mix(ull o){
+    o+=0x9e3779b97f4a7c15;
+    o=(o^(o>>30))*0xbf58476d1ce4e5b9;
+    o=(o^(o>>27))*0x94d049bb133111eb;
+    return o^(o>>31);
+    //Those constants supposedly are chosen to give this function better pseudo-random properties, 
+    but on any on-site contest, when one can't have team reference document/doesn't want to waste time
+    searching it for implementation typing arbitrary large odd numbers by hand should be good enough
+}
+
+And value used for x is mix(x), or in case of rounds with hacking mix(x ^ salt), where salt is value
+returned by chrono::steady_clock::now().time_since_epoch().count() at the beginning of runtime. This
+is way faster than anything using modulo, uses all 2^64 possible hash values, so avoids any birthday
+paradox issues, also avoids any overflows/issues with and requires way less code than using more than one modulo.
+
+Moreover this technique can be used for hashing other objects than sets, e.g. sequences, which can be represented
+as set of pairs (i,ai), the only difference is that now we need random value for each pair, but this can be easily
+done e.g. by
+
+ull hash(pii a) {return mix(a.first ^ mix(a.second));}
+
+And finally: using well-known modulos is not an issue, as long as you use random number as base 
+(guaranteed by Schwartz–Zippel lemma).
+*/
+
 // Gracias el vasito
 struct Hash {
 	int P=1777771,MOD[2],PI[2];
