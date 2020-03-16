d0fddc4606620044c1bb37802349321fcf99f3d9
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-03-16 17:36:39 -0500

    Update ChordalGraph.cpp

--- Estructuras y algoritmos/ChordalGraph.cpp
+++ Estructuras y algoritmos/ChordalGraph.cpp
@@ -11,38 +11,38 @@ struct ChordGraph{
 		memset(l, 0, 4*n);
 		r[0]=n-1;
 		memset(c, 0, 4*n);
-		for(int i=n-1; i >= 0; --i) {
-			int u=qu[i];
+		for(int i = n-1; i >= 0; --i) {
+			int u = qu[i];
 			--r[l[i]];
 			vector<array<int, 2>> z;
 			for(int v : adj[u]) {
-				if(pos[v]>i) continue;
-				int lc=l[pos[v]];
+				if(pos[v] > i) continue;
+				int lc = l[pos[v]];
 				z.push_back({r[lc]-c[lc]++, r[lc]});
 			}
 			for(int v : adj[u]) {
-				if(pos[v]>i) continue;
-				int np=r[l[pos[v]]]--;
-				pos[qu[np]]=pos[v];
+				if(pos[v] > i) continue;
+				int np = r[l[pos[v]]]--;
+				pos[qu[np]] = pos[v];
 				swap(qu[pos[v]], qu[np]);
-				l[np]=np-(--c[l[pos[v]]]);
-				pos[v]=np;
+				l[np] = np-(--c[l[pos[v]]]);
+				pos[v] = np;
 			}
 			for(array<int, 2> a : z)
-				r[a[0]]=a[1];
-			int w=-1;
+				r[a[0]] = a[1];
+			int w = -1;
 			for(int v : adj[u])
 				if(pos[v]>i && (w<0 || pos[v]<pos[w]))
 					w=v;
 			if(w >= 0)
 				a[w].push_back(u);
 		}
-		for(int i=0; i<n; ++i) {
+		for(int i = 0; i < n; ++i) {
 			for(int j : adj[i])
 				c[j]=1;
 			for(int j : a[i])
 				for(int k : adj[j])
-					if(pos[k]>pos[i]&&!c[k])
+					if(pos[k]>pos[i] && !c[k])
 						return 0;
 			for(int j : adj[i])
 				c[j]=0;
