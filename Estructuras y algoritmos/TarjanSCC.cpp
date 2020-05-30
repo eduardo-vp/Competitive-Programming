752070ed505be69459b7adbf8b0f6a28be478ece
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-05-29 23:11:13 -0500

    Create TarjanSCC.cpp

---
+++ Estructuras y algoritmos/TarjanSCC.cpp
@@ -0,0 +1,29 @@
+int comp[N];
+vector<int> nodes[N];
+vector<int> adj[N];
+
+int Tarjan(int n){
+	int gid = 0, scc = 0;
+	vector<int> num(n+1,0), low(n+1,0), vist(n+1,0), stk;
+	function<void(int)> dfs = [&](int u){
+		num[u] = low[u] = ++gid;
+		stk.push_back(u);
+		vist[u] = 1;
+		for(int v : adj[u]){
+			if(num[v] == 0) dfs(v);
+			if(vist[v]) low[u] = min(low[u],low[v]);
+		}
+		if(num[u] == low[u]){
+			int v;
+			do{
+				v = stk.back(); stk.pop_back(); vist[v] = 0;
+				nodes[scc].push_back(v);
+			}while(u != v);
+			scc++;
+		}
+	};
+	for(int i = 1; i <= n; ++i){
+		if(!num[i]) dfs(i);
+	}
+	return scc;
+}
