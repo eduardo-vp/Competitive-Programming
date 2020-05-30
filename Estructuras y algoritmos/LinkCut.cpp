ed3ed0c163308d967ce7a0ce3d87bf60ee079a2d
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-05-30 00:25:40 -0500

    Update LinkCut.cpp

--- Estructuras y algoritmos/LinkCut.cpp
+++ Estructuras y algoritmos/LinkCut.cpp
@@ -2,7 +2,7 @@
 // Usage:
 // Node* x[N];
 // for(int i = 0; i < n; ++i)
-//   x[i] = new Node(0);
+//   x[i] = new Node();
 // connect a,b -> link(x[a], x[b])
 // cut a,b -> cut(x[a], x[b])
 // check connectivity -> connected(x[a], x[b])
@@ -10,25 +10,28 @@
 struct Node{
   bool rev;
   Node *l, *r, *p;
-  Node(int v) : rev(0), l(0), r(0), p(0){ }
-  bool isRoot(){return !p || (p->l != this && p->r != this);}
+  Node() : rev(0), l(0), r(0), p(0){ }
+  bool isRoot(){ return !p || (p->l != this && p->r != this);}
   void push(){
     if(rev){
-      rev=0;
+      rev = 0;
       swap(l, r);
-      if(l)l->rev=!l->rev; if(r)r->rev=!r->rev;
+      if(l) l->rev = !l->rev; 
+	  if(r) r->rev = !r->rev;
     }
   }
 };
 
 void connect(Node *ch, Node *p, int isLeftChild){
-  if(ch) ch->p=p; if(isLeftChild<0)return;
-  if(isLeftChild) p->l=ch; else p->r = ch;
+  if(ch) ch->p = p; 
+  if(isLeftChild < 0) return;
+  if(isLeftChild) p->l = ch; 
+  else p->r = ch;
 }
 
 void rotate(Node *x){
   Node *p = x->p, *g = p->p;
-  bool isRootP = p->isRoot(), leftChildX = x==p->l;
+  bool isRootP = p->isRoot(), leftChildX = (x == p->l);
   connect(leftChildX ? x->r : x->l, p, leftChildX);
   connect(p, x, !leftChildX);
   connect(x, g, isRootP ? -1 : (p == g->l));
@@ -37,24 +40,43 @@ void rotate(Node *x){
 void splay(Node *x){
   while(!x->isRoot()){
     Node *p = x->p, *g = p->p;
-    if(!p->isRoot())g->push();
-    p->push(); x->push();
-    if(!p->isRoot())rotate((x == p->l) == (p == g->l) ? p : x);
+    if(!p->isRoot()) g->push();
+    p->push(); 
+	x->push();
+    if(!p->isRoot()) rotate((x == p->l) == (p == g->l) ? p : x);
     rotate(x);
   }
   x->push();
 }
 
 Node* expose(Node *x){
-  Node *last=0;
-  for(Node *y=x; y; y=y->p) splay(y), y->l=last, last=y;
+  Node *last = 0;
+  for(Node *y = x; y; y = y->p) 
+  	splay(y), y->l = last, last = y;
   splay(x);
   return last;
 }
 
-void makeRoot(Node *x){expose(x); x->rev = !x->rev;}
-bool connected(Node *x, Node *y){if(x==y)return 1; expose(x); expose(y); return x->p != 0;}
-void link(Node *x, Node *y){makeRoot(x); x->p=y;}
+void makeRoot(Node *x){
+	expose(x); 
+	x->rev = !x->rev;
+}
+
+bool connected(Node *x, Node *y){
+	if(x == y) return true; 
+	expose(x); 
+	expose(y); 
+	return x->p != 0;
+}
+
+void link(Node *x, Node *y){
+	makeRoot(x); 
+	x->p = y;
+}
+
 void cut(Node *x, Node *y){
-  makeRoot(x); expose(y); y->r->p = 0; y->r=0;
+  makeRoot(x); 
+  expose(y); 
+  y->r->p = 0; 
+  y->r = 0;
 }
