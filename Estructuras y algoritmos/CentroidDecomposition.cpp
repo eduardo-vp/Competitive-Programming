5d135092ec83416cd97e33ea5689e20e5fda6dcb
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-05-31 18:32:35 -0500

    Update CentroidDecomposition.cpp

--- Estructuras y algoritmos/CentroidDecomposition.cpp
+++ Estructuras y algoritmos/CentroidDecomposition.cpp
@@ -22,7 +22,6 @@ vector<int> adj[N];
 struct CentroidD{
 	int n;
 	int cpar[N],tsz[N];
-	
 	int dfs(int u, int p){
 		tsz[u] = 1;
 		for(int v : adj[u]) if(v != p and cpar[v] == -1){
@@ -32,18 +31,14 @@ struct CentroidD{
 	}
 
 	void decomp(int u, int p, int sb, int prevc){
-		for(int v : adj[u]){
-			if(v != p and cpar[v] == -1 and 2*tsz[v] > sb){
-				decomp(v,u,sb,prevc);
-				return;
-			}
+		for(int v : adj[u]) if(v != p and cpar[v] == -1 and 2*tsz[v] > sb){
+			decomp(v,u,sb,prevc);
+			return;
 		}
 		cpar[u] = prevc; // careful if prevc == -2
-		for(int v : adj[u]){
-			if(cpar[v] == -1){
-				dfs(v,u);
-				decomp(v,u,tsz[v],u);
-			}
+		for(int v : adj[u]) if(cpar[v] == -1){
+			dfs(v,u);
+			decomp(v,u,tsz[v],u);
 		}
 	}
 	
