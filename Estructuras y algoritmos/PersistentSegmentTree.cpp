d45d46662f8e6de9ff5351deedcb2a9bfe408b9e
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-06-01 23:54:37 -0500

    Create PersistentSegmentTree.cpp

---
+++ Estructuras y algoritmos/PersistentSegmentTree.cpp
@@ -0,0 +1,41 @@
+// Gracias El vasito
+const int NEUT = 1e9;
+
+struct STree{ // persistent segment tree for min over integers
+	int n,rt;
+	vector<int> st,left,right;
+	STree(int n) : st(1,NEUT), n(n), rt(0){}
+	int comb(int a, int b){
+		return min(a,b);
+	}
+	int new_node(int v, int l = 0, r = 0){
+		int ks = sz(st);
+		st.pb(v); left.pb(l); right.pb(r);
+		return ks;
+	}
+	int init(int s, int e, int *a){ // not necessary in most cases
+		if(s+1 == e) return new_node(a[s]);
+		int m = (s+e)/2, l = init(s,m,a), r = init(m,e,a);
+		return new_node(comb(st[l],st[r]),l,r);
+	}
+	int update(int k, int s, int e, int p, int v){
+		int ks = new_node(st[k],left[k],right[k]);
+		if(s+1 == e){ st[ks] = v; return ks; }
+		int m = (s+e)/2, ps;
+		if(p < m) ps = update(L[ks],s,m,p,v), left[ks] = ps;
+		else ps = update(R[ks],m,e,p,v), right[ks] = ps;
+		st[ks] = comb(st[left[ks]],st[right[ks]]);
+		return ks;
+	}
+	int query(int k, int s, int e, int a, int b){
+		if(e <= a || b <= s) return NEUT;
+		if(a <= s && e <= b) return st[k];
+		int m = (s+e)/2;
+		return comb(query(left[k],s,m,a,b),query(right[k],m,e,a,b));
+	}
+	int init(int *a){ return init(0,n,a); }
+	int update(int k, int p, int v){ return rt = update(k,0,n,p,v); }
+	int update(int p, int v){ return update(rt,p,v); } // update on last root
+	int query(int k, int a, int b){ return query(k,0,n,a,b); }
+}; 
+// usage: STree rmq(n); root = rmq.init(x); new_root = rmq.update(root,i,v); rmq.query(root,s,e);
