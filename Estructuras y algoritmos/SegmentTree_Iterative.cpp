7cd87869f2fe966486d359ad947b1cc96aec7320
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-06-02 23:34:33 -0500

    Update and rename segmentTree.cpp to SegmentTree_Iterative.cpp

--- Estructuras y algoritmos/SegmentTree_Iterative.cpp
+++ Estructuras y algoritmos/SegmentTree_Iterative.cpp
@@ -1,9 +1,6 @@
 
 struct MaxST{
 	int n, t[2*N];
-	
-	MaxST(){}
-	
 	void init(int m, vector<int> &vec){
 		n = m;
 		for(int i = 0; i < n; ++i)
@@ -11,7 +8,6 @@ struct MaxST{
 		for(int i = n-1; i > 0; --i)
 			t[i] = max(t[i<<1],t[i<<1|1]);
 	}
-	
 	int query(int l, int r){
 		int ans = INT_MIN;
 		for(l += n, r += n; l < r; l >>= 1, r >>= 1){
@@ -20,7 +16,6 @@ struct MaxST{
 		}
 		return ans;
 	}
-	
 	void update(int p, int v){
 		for(t[p+=n] = v; p >>= 1; )
 			t[p] = max(t[p<<1],t[p<<1|1]);
