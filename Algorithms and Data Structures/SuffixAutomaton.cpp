d2fc695a9d00b2f02735c90a02840a24145222ce
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-06-22 19:04:40 -0500

    Create SuffixAutomaton.cpp

---
+++ Algorithms and Data Structures/SuffixAutomaton.cpp
@@ -0,0 +1,57 @@
+
+// implementation for lowercase letters only
+
+const int ALPH = 30;
+
+struct state{
+	int len,link;
+	int next[ALPH];
+	state(){ memset(next,-1,sizeof(next)); }
+};
+
+struct SuffixAutomaton{
+	int cnt,last;
+	vector<state> st;
+	int f(char ch){ return ch - 'a'; }
+
+	void extend(char ch){
+		int now = cnt++, c = f(ch);
+		st[now].len = st[last].len + 1;
+		int p = last;
+		while(p != -1 && st[p].next[c] == -1){
+			st[p].next[c] = now;
+			p = st[p].link;
+		}
+		if(p == -1){
+			st[now].link = 0;
+		}else{
+			int q = st[p].next[c];
+			if(st[p].len + 1 == st[q].len){
+				st[now].link = q;
+			}else{
+				int clone = cnt++;
+				st[clone].len = st[p].len + 1;
+				st[clone].link = st[q].link;
+				memcpy(&st[clone].next, &st[q].next, sizeof(st[q].next));
+				while(p != -1 && st[p].next[c] == q){
+					st[p].next[c] = clone;
+					p = st[p].link;
+				}
+				st[q].link = st[now].link = clone;
+			}
+		}
+		last = now;
+	}
+
+	SuffixAutomaton(string &s){
+		int n = s.size();
+		st.resize(2*n + 5);
+		st[0].len = 0;
+		st[0].link = -1;
+		cnt = 1;
+		last = 0;
+		for(char ch : s){
+			extend(ch);
+		}
+	}
+};
