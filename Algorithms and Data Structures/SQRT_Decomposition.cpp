e8e044e74851ae1c678f42abc612740a85b0a7c9
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-07-22 00:46:37 -0500

    Create SQRT_Decomposition.cpp

---
+++ Algorithms and Data Structures/SQRT_Decomposition.cpp
@@ -0,0 +1,59 @@
+
+// SQ * BL > N
+// SQ = Block size
+// BL = Number of blocks
+const int SQ = 2000, N = 1e5 + 100, BL = 52;
+
+int id[N];
+
+// interval [l,r)
+void update(int l, int r, int x){
+	if(id[l] == id[r]){
+		for(int i = l; i < r; ++i){
+			// update position
+		}
+	}else{
+		while(l % SQ){
+			// update position
+			l++;
+		}
+		while(r % SQ){
+			r--;
+      // update position
+		}
+		for(int i = id[l]; i < id[r]; ++i){
+			// update block
+		}
+	}
+}
+
+// interval [l,r)
+int query(int l, int r){
+	int ans = 0;
+	if(id[l] == id[r]){
+		for(int i = l; i < r; ++i){
+			// query position
+		}
+	}else{
+		while(l % SQ){
+			// query position
+			l++;
+		}
+		while(r % SQ){
+			r--;
+			// query position
+		}
+		for(int i = id[l]; i < id[r]; ++i){
+			// query block
+		}
+	}
+	return ans;
+}
+
+int main(){
+
+	fastio;
+	REP(i,N) id[i] = i/SQ;
+  
+	return 0;
+}
