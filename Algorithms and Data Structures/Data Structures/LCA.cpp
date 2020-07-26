1ec2ba4ef4bd1d1752dfbcc2c599bcaa75c0555a
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-07-26 17:00:18 -0500

    Update LCA.cpp

--- Algorithms and Data Structures/Data Structures/LCA.cpp
+++ Algorithms and Data Structures/Data Structures/LCA.cpp
@@ -1,33 +1,59 @@
-  
 
+// N maximum number of nodes
+// LG >= log(N)
+
+int dep[N];
+int P[N][LG];
 
 int __lca(int a, int b){
 	if(dep[a] < dep[b]) swap(a,b);
 	for(int j = LG-1; j >= 0; --j)
 		if( dep[a]-(1<<j) >= dep[b] )
-			a = p[a][j];
+			a = P[a][j];
 	if(a == b) return a;
 	for(int j = LG-1; j >= 0; --j)
-		if(p[a][j] != p[b][j])
-			a = p[a][j], b = p[b][j];
-	return pai[a];
+		if(P[a][j] != P[b][j])
+			a = P[a][j], b = P[b][j];
+	return P[a][0];
 }
 
-int dist(int a, int b){
+int get_dist(int a, int b){
 	int lca = __lca(a,b);
 	return dep[a] + dep[b] - 2 * dep[lca];
 }
 
-void build(){
-	for(int i = 1; i <= n; ++i)
-		for(int j = 0; j < LG; ++j)
-			p[i][j] = -1;
+void dfs(int u, int p = -1, int d = 0){
+	dep[u] = d;
+	P[u][0] = p;
+	for(int i = 1; i < LG && P[u][i-1] >= 0; ++i){
+		P[u][i] = P[P[u][i-1]][i-1];
+	}
+	for(int v : adj[u]) if(v != p){
+		dfs(v,u,d+1);
+	}
+}
+
+// returns the k-th node in the path from u to v (0-indexed) 
+int get_kth(int u, int v, int lca, int k){
+	int d = dep[u] - dep[lca];
+	if(k <= d){
+		for(int i = LG-1; i >= 0; --i){
+			if(k - (1 << i) >= 0){
+				k -= (1 << i);
+				u = P[u][i];
+			}
+		}
+		return u;
+	}
+	int dd = dep[u] + dep[v] - 2*dep[lca];
+	return get_kth(v,u,lca,dd-k);
+}
+
+int main(){
 	
-	for(int i = 1; i <= n; ++i)
-		p[i][0] = pai[i];
+	// read graph
+	memset(P,-1,sizeof(P));
+	dfs(root);
 	
-	for(int j = 1; j < LG; ++j)
-		for(int i = 1; i <= n; ++i)
-			if(p[i][j-1] != -1)
-				p[i][j] = p[p[i][j-1]][j-1];
+	return 0;	
 }
