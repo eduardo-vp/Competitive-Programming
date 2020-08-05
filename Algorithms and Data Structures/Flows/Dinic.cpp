3cb8dc76a0b89873d6d5820e5a5d98b8e9e09748
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-08-04 20:48:29 -0500

    Update Dinic.cpp

--- Algorithms and Data Structures/Flows/Dinic.cpp
+++ Algorithms and Data Structures/Flows/Dinic.cpp
@@ -1,5 +1,5 @@
-/*   Dinic Max Flow
-   Autor: Matias Hunicken
+/* Gracias El vasito  
+   Dinic Max Flow
    Intrucciones de uso:
       Min cut: nodes with dist>=0 vs nodes with dist<0
       Matching MVC: left nodes with dist<0 + right nodes with dist>0
@@ -8,45 +8,50 @@
 struct Dinic{
    int nodes,src,dst;
    vector<int> dist,q,work;
-   struct edge {int to,rev;ll f,cap;};
+   struct edge {int to,rev; ll f,cap;};
    vector<vector<edge>> g;
    Dinic(int x):nodes(x),g(x),dist(x),q(x),work(x){}
    void add_edge(int s, int t, ll cap){
-      g[s].pb((edge){t,SZ(g[t]),0,cap});
-      g[t].pb((edge){s,SZ(g[s])-1,0,0});
+      g[s].pb((edge){t,sz(g[t]),0,cap});
+      g[t].pb((edge){s,sz(g[s])-1,0,0});
    }
    bool dinic_bfs(){
-      fill(ALL(dist),-1);dist[src]=0;
-      int qt=0;q[qt++]=src;
-      for(int qh=0;qh<qt;qh++){
-         int u=q[qh];
-         FER(i,0,SZ(g[u])){
-            edge &e=g[u][i];int v=g[u][i].to;
-            if(dist[v]<0&&e.f<e.cap)
-               dist[v]=dist[u]+1,q[qt++]=v;
+      dist[src] = 0;
+      fill(dist.begin(),dist.end(),-1);
+      int qt = 0; q[qt++] = src;
+      for(int qh = 0;qh < qt; qh++){
+         int u = q[qh];
+         for(int i = 0; i < sz(g[u]); ++i){
+            edge &e = g[u][i]; int v = g[u][i].to;
+            if(dist[v] < 0 && e.f < e.cap)
+               dist[v] = dist[u]+1, q[qt++]=v;
          }
       }
-      return dist[dst]>=0;
+      return dist[dst] >= 0;
    }
    ll dinic_dfs(int u, ll f){
-      if(u==dst)return f;
-      for(int &i=work[u];i<SZ(g[u]);i++){
-         edge &e=g[u][i];
-         if(e.cap<=e.f)continue;
-         int v=e.to;
-         if(dist[v]==dist[u]+1){
-            ll df=dinic_dfs(v,min(f,e.cap-e.f));
-            if(df>0){e.f+=df;g[v][e.rev].f-=df;return df;}
+      if(u == dst) return f;
+      for(int &i = work[u]; i< sz(g[u]); ++i){
+         edge &e = g[u][i];
+         if(e.cap <= e.f)continue;
+         int v = e.to;
+         if(dist[v] == dist[u]+1){
+            ll df = dinic_dfs(v,min(f,e.cap-e.f));
+            if(df>0){ 
+				e.f += df; 
+				g[v][e.rev].f -= df; 
+				return df;
+			}
          }
       }
       return 0;
    }
    ll max_flow(int _src, int _dst){
-      src=_src;dst=_dst;
-      ll result=0;
+      src=_src; dst=_dst;
+      ll result = 0;
       while(dinic_bfs()){
-         fill(ALL(work),0);
-         while(ll delta=dinic_dfs(src,INF))result+=delta;
+         fill(work.begin(),work.end(),0);
+         while(ll delta = dinic_dfs(src,INF)) result+=delta;
       }
       return result;
    }
