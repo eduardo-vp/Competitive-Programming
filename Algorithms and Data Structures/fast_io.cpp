dc84b5aa1a39c9570e920659338385f7734fdda5
Emanuel Velarde <32459232+eduardovp97@users.noreply.github.com>
2020-08-16 17:20:18 -0500

    Create fast_io.cpp

---
+++ Algorithms and Data Structures/fast_io.cpp
@@ -0,0 +1,11 @@
+
+template<class num> inline void rd(num &x) {
+	char c;
+	while(isspace(c = getchar()));
+	bool neg = false;
+	if(!isdigit(c)) neg = (c == '-'), x = 0;
+	else x = c - '0';
+	while(isdigit(c = getchar()))
+		x = (x << 3) + (x << 1) + c - '0';
+	if(neg) x = -x;
+}
