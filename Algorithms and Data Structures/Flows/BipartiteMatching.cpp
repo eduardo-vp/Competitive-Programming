c122655837cd5109d2c8b0e0f78c7cf895fcc970
Eduardo Velarde <eduardo.velarde@pucp.pe>
2021-11-09 00:18:13 -0500

    Update Biparte Matching

--- Algorithms and Data Structures/Flows/BipartiteMatching.cpp
+++ Algorithms and Data Structures/Flows/BipartiteMatching.cpp
@@ -1,85 +1,67 @@
-// i will show you da wae
-#include <bits/stdc++.h>
-#define mp make_pair
-#define pb push_back
-#define fill(x,v) memset(x,v,sizeof(x))
-#define FOR(i,a,b) for(int i = int(a); i < int(b); ++i)
-#define trace(x) cout << #x << " = " << x << endl
-#define fastio ios_base::sync_with_stdio(0);cin.tie(0)
-#define N 520
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> ii;
-const int INF = 1e9;
 
-int n,m,nil,match[N],dist[N];
-vector<int> adj[N];
+const int inf = 1e9;
 
-bool bfs(){
-  queue<int> q;
-  for(int i = 1; i <= n; ++i){
-    if(match[i] == nil){
-      dist[i] = 0;
-      q.push(i);
-    }else dist[i] = INF;
+struct BipMatching {
+  int n, m, nil;
+  vector<int> match, dist;
+  vector< vector<int> > adj;
+  BipMatching(int n, int m): n(n), m(m) {
+    nil = n + m + 2;
+    match.resize(nil + 1);
+    dist.resize(nil + 1);
+    adj.resize(nil + 1);
   }
-  dist[nil] = INF;
-  while(!q.empty()){
-    int u = q.front();
-    q.pop();
-    if(u == nil) continue;
-    for(int v : adj[u]){
-      if(dist[match[v]] == INF){
-        dist[match[v]] = dist[u] + 1;
-        q.push(match[v]);
+  void add_edge(int x, int y) {
+    adj[x].push_back(y);
+  }
+  bool bfs() {
+    queue<int> q;
+    for (int i = 0; i < n; ++i) {
+      if (match[i] == nil) {
+        dist[i] = 0;
+        q.push(i);
+      } else dist[i] = inf;
+    }
+    dist[nil] = inf;
+    while (!q.empty()) {
+      int u = q.front(); q.pop();
+      if (u == nil) continue;
+      for (int v: adj[u]) {
+        if (dist[match[v]] == inf) {
+          dist[match[v]] = dist[u] + 1;
+          q.push(match[v]);
+        }
       }
     }
+    return dist[nil] != inf;
   }
-  return dist[nil] != INF;
-}
-
-bool dfs(int u){
-  if(u != nil){
-    for(int v : adj[u]){
-      if(dist[match[v]] != dist[u] + 1) continue;
-      if(dfs(match[v])){
-        match[v] = u;
-        match[u] = v;
-        return true;
+  bool dfs(int u) {
+    if (u != nil) {
+      for (int v: adj[u]) {
+        if (dist[match[v]] != dist[u] + 1) continue;
+        if (dfs(match[v])) {
+          match[v] = u;
+          match[u] = v;
+          return true;
+        }
+      }
+      dist[u] = inf;
+      return false;
+    }
+    return true;
+  }
+  int solve() {
+    for (int i = 0; i < n + m; ++i) {
+      match[i] = nil;
+    }
+    int matching = 0;
+    while (bfs()) {
+      for (int i = 0; i < n; ++i) {
+        if (match[i] == nil && dfs(i)) {
+          matching++;
+        }
       }
     }
-    dist[u] = INF;
-    return false;
+    return matching;
   }
-  return true;
-}
-
-int bipMatching(){
-  nil = n + m + 1;
-  for(int i = 1; i <= n+m; ++i)
-    match[i] = nil;
-  int matching = 0;
-  while(bfs()){
-    for(int i = 1; i <= n; ++i)
-      if(match[i] == nil and dfs(i))
-        ++matching;
-  }
-  return matching;
-}
-
-int main(){
-    
-  int x;
-  cin >> n >> m;
-  FOR(i,0,n){
-    while(cin >> x and x != 0)
-      adj[i+1].pb(n+x);
-  }
-
-  cout << bipMatching() << endl;
-  for(int i = 1; i <= n; ++i)
-    if(match[i] != nil)
-      cout << i << " " << match[i]-n << endl;
-
-    return 0;
-}
+};
