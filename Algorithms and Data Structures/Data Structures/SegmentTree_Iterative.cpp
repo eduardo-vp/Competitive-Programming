788cc248ae4899edcf1e87feda8275d750a48a7a
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2021-11-28 12:55:00 -0500

    Update SegmentTree_Iterative.cpp

--- Algorithms and Data Structures/Data Structures/SegmentTree_Iterative.cpp
+++ Algorithms and Data Structures/Data Structures/SegmentTree_Iterative.cpp
@@ -3,17 +3,18 @@ struct STree{
   int n;
   vector<int> t;
   STree(int n): n(n), t(2 * n + 5) {}
-  void build(vector<int> &vec) {
-    assert(vec.size() == n);
+  STree(vector<int> &a) {
+    n = a.size();
+    t.assign(2 * n + 5, 0);
     for (int i = 0; i < n; ++i) {
-      t[n + i] = vec[i];
+      t[i + n] = a[i];
     }
     for (int i = n - 1; i > 0; --i) {
       t[i] = max(t[i << 1], t[i << 1 | 1]);
     }
   }
   int query(int l, int r) {
-    int ans = INT_MIN;
+    int ans = -1;
     for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
       if (l & 1) ans = max(ans, t[l++]);
       if (r & 1) ans = max(ans, t[--r]);
