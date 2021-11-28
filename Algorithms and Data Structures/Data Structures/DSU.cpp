10754da0a854ef9ac785f7da19f89046a1a4edb1
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2021-11-28 12:52:44 -0500

    Update DSU.cpp

--- Algorithms and Data Structures/Data Structures/DSU.cpp
+++ Algorithms and Data Structures/Data Structures/DSU.cpp
@@ -1,6 +1,28 @@
-struct UF{
-  int p[N];
-  UF(){ REP(i,0,N) p[i] = i; }
-  int find(int i){ return (p[i] == i) ? i : (p[i] = find(p[i])); }
-  void join(int i, int j){ p[find(i)] = find(j); }
+
+struct Dsu {
+  int n;
+  vector<int> p;
+  vector<int> sz;
+
+  Dsu(int n) : n(n) {
+    p.resize(n);
+    iota(p.begin(), p.end(), 0);
+    sz.assign(n, 1);
+  }
+
+  inline int find(int x) {
+    return (x == p[x] ? x : (p[x] = find(p[x])));
+  }
+
+  inline bool unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x != y) {
+      p[x] = y;
+      sz[y] += sz[x];
+      sz[x] = 0;
+      return true;
+    }
+    return false;
+  }
 };
