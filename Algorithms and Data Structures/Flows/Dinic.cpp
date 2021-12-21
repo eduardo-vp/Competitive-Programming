4fb2b6843737ce036f95d79fab754a82d3f4a918
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2021-12-20 22:38:59 -0500

    Update Dinic.cpp

--- Algorithms and Data Structures/Flows/Dinic.cpp
+++ Algorithms and Data Structures/Flows/Dinic.cpp
@@ -5,39 +5,42 @@
       Matching MVC: left nodes with dist<0 + right nodes with dist>0
 */
 
+typedef long long tf; // flow type
+const tf inf = 2e18;
+
 struct Dinic{
-  int nodes,src,dst;
-  vector<int> dist,q,work;
-  struct edge {int to,rev; ll f,cap;};
+  int nodes, src, dst;
+  vector<int> dist, q, work;
+  struct edge { int to, rev; tf f, cap; };
   vector<vector<edge>> g;
-  Dinic(int x):nodes(x),g(x),dist(x),q(x),work(x){}
-  void add_edge(int s, int t, ll cap){
-    g[s].push_back((edge){t,int(g[t].size()),0,cap});
-    g[t].push_back((edge){s,int(g[s].size())-1,0,0});
+  Dinic(int x): nodes(x), g(x), dist(x), q(x), work(x) {}
+  void add_edge(int s, int t, tf cap) {
+    g[s].push_back((edge){t, int(g[t].size()), 0, cap});
+    g[t].push_back((edge){s, int(g[s].size()) - 1, 0, 0});
   }
   bool dinic_bfs(){
     fill(dist.begin(),dist.end(),-1);
     dist[src] = 0;
     int qt = 0; q[qt++] = src;
-    for(int qh = 0;qh < qt; qh++){
+    for (int qh = 0;qh < qt; qh++) {
       int u = q[qh];
-      for(int i = 0; i < g[u].size(); ++i){
+      for (int i = 0; i < g[u].size(); ++i) {
         edge &e = g[u][i]; int v = g[u][i].to;
-        if(dist[v] < 0 && e.f < e.cap)
-          dist[v] = dist[u]+1, q[qt++]=v;
+        if (dist[v] < 0 && e.f < e.cap)
+          dist[v] = dist[u] + 1, q[qt++] = v;
       }
     }
     return dist[dst] >= 0;
   }
-  ll dinic_dfs(int u, ll f){
+  tf dinic_dfs(int u, tf f){
     if(u == dst) return f;
-    for(int &i = work[u]; i < g[u].size(); ++i){
+    for (int &i = work[u]; i < g[u].size(); ++i) {
       edge &e = g[u][i];
-      if(e.cap <= e.f)continue;
+      if (e.cap <= e.f) continue;
       int v = e.to;
-      if(dist[v] == dist[u]+1){
-        ll df = dinic_dfs(v,min(f,e.cap-e.f));
-        if(df>0){
+      if (dist[v] == dist[u] + 1) {
+        tf df = dinic_dfs(v, min(f, e.cap - e.f));
+        if (df > 0) {
           e.f += df;
           g[v][e.rev].f -= df;
           return df;
@@ -46,12 +49,12 @@ struct Dinic{
     }
     return 0;
   }
-  ll max_flow(int _src, int _dst){
+  tf max_flow(int _src, int _dst){
     src=_src; dst=_dst;
-    ll result = 0;
-    while(dinic_bfs()){
-      fill(work.begin(),work.end(),0);
-      while(ll delta = dinic_dfs(src,INF)) result+=delta;
+    tf result = 0;
+    while (dinic_bfs()) {
+      fill(work.begin(), work.end(), 0);
+      while(tf delta = dinic_dfs(src, inf)) result += delta;
     }
     return result;
   }
