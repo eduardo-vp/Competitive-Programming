dbb0e3136fe47aa985b6a4c89769589070b9165a
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2022-02-08 18:49:48 -0500

    Update Eulerian_Path.cpp

--- Algorithms and Data Structures/Graphs/Eulerian_Path.cpp
+++ Algorithms and Data Structures/Graphs/Eulerian_Path.cpp
@@ -1,29 +1,41 @@
 
 // Gracias El vasito
 // Directed version (uncomment commented code for undirected)
-struct edge {
-  int y;
-//  list<edge>::iterator rev;
-  edge(int y):y(y){}
-};
-list<edge> g[MAXN];
-void add_edge(int a, int b){
-  g[a].push_front(edge(b));//auto ia=g[a].begin();
-//  g[b].push_front(edge(a));auto ib=g[b].begin();
-//  ia->rev=ib;ib->rev=ia;
-}
-vector<int> p;
-void go(int x){
-  while(g[x].size()){
-    int y=g[x].front().y;
-    //g[y].erase(g[x].front().rev);
-    g[x].pop_front();
-    go(y);
+struct EulerianPath {
+  struct edge {
+    int y;
+    // list<edge>::iterator rev;
+    edge(int y): y(y) {}
+  };
+ 
+  vector<int> p;
+  vector< list<edge> > adj;
+ 
+  EulerianPath(int n): adj(n) {}
+ 
+  void add_edge(int a, int b) {
+    adj[a].push_front(edge(b));
+    // auto ia = adj[a].begin();
+    // adj[b].push_front(edge(a));
+    // auto ib = adj[b].begin();
+    // ia->rev = ib; ib->rev = ia;
   }
-  p.push_back(x);
-}
-vector<int> get_path(int x){ // get a path that begins in x
-// check that a path exists from x before calling to get_path!
-  p.clear();go(x);reverse(p.begin(),p.end());
-  return p;
-}
+ 
+  void go(int x) {
+    while (adj[x].size()) {
+      int y = adj[x].front().y;
+      // adj[y].erase(adj[x].front().rev);
+      adj[x].pop_front();
+      go(y);
+    }
+    p.push_back(x);
+  }
+ 
+  vector<int> get_path(int x) { // get a path that begins in x
+    p.clear(); // clear if get_path was previously called
+    go(x);
+    reverse(p.begin(), p.end());
+    return p;
+  }
+};
+ 
