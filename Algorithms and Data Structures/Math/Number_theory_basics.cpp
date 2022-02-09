e17cb665dd6a19f25ef7feb5cc5f7ba7d5def2fb
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2022-02-08 22:53:58 -0500

    Update Number_theory_basics.cpp

--- Algorithms and Data Structures/Math/Number_theory_basics.cpp
+++ Algorithms and Data Structures/Math/Number_theory_basics.cpp
@@ -4,10 +4,10 @@ int extended_euclid(int a, int b, int &x, int &y) {
   int xx = y = 0;
   int yy = x = 1;
   while (b) {
-    int q = a/b;
-    int t = b; b = a %b; a = t;
-    t = xx; xx = x-q*xx; x = t;
-    t = yy; yy = y-q*yy; y = t;
+    int q = a / b;
+    int t = b; b = a % b; a = t;
+    t = xx; xx = x - q * xx; x = t;
+    t = yy; yy = y - q * yy; y = t;
   }
   return a;
 }
