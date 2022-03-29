42a0b4cb4a15fc76e735c0084fff1cb0a52a4bad
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2022-03-29 11:58:49 -0500

    Create Bit2D.cpp

---
+++ Algorithms and Data Structures/Data Structures/Bit2D.cpp
@@ -0,0 +1,39 @@
+
+template<typename T>
+struct Bit2D {
+  int n, m;
+  vector< vector<int> > bit;
+ 
+  Bit2D(int n, int m): n(n), m(m) {
+    bit.resize(n);
+    for (int i = 0; i < n; ++i) {
+      bit[i].resize(m);
+    }
+  }
+ 
+  inline void modify(int i, int j, T v) {
+    int x = i;
+    while (x < n) {
+      int y = j;
+      while (y < m) {
+        bit[x][y] += v;
+        y |= (y + 1);
+      }
+      x |= (x + 1);
+    }
+  }
+ 
+  inline T get(int i, int j) {
+    T v{};
+    int x = i;
+    while (x >= 0) {
+      int y = j;
+      while (y >= 0) {
+        v += bit[x][y];
+        y = (y & (y + 1)) - 1;
+      }
+      x = (x & (x + 1)) - 1;
+    }
+    return v;
+  }
+};
