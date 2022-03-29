b7c7ee337553fb2096b44d5301d51c3769dd18a8
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2022-03-29 13:02:06 -0500

    Update Dynamic_Connectivity2.cpp

--- Algorithms and Data Structures/Graphs/Dynamic_Connectivity2.cpp
+++ Algorithms and Data Structures/Graphs/Dynamic_Connectivity2.cpp
@@ -1,18 +1,6 @@
 /* Solves dynamic connectivity offline in O((n+m) sqrt(m log n)) */
 /* queries : add, rem, connected? */
 
-// cities to vapori-
-// corner cases // int vs ll // cin vs scanf // clear structures // statement // doublesz
-#include <bits/stdc++.h>
-#define pb push_back
-#define fst first
-#define snd second
-#define sz(x) int(x.size())
-#define fill(x,v) memset(x,v,sizeof(x))
-#define REP(i,a,b) for(int i = int(a); i < int(b); ++i)
-#define trace(x) cout << #x << " = " << x << endl
-#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-using namespace std;
 typedef long long ll;
 typedef pair<int,int> ii;
 const int MOD = 1e9+7;
