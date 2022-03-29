be279201b83376fd59e87f7b5128bd4625b0edb5
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2022-03-29 13:07:33 -0500

    Update Random.cpp

--- Algorithms and Data Structures/Random.cpp
+++ Algorithms and Data Structures/Random.cpp
@@ -1,15 +1,3 @@
-// dile a la jardinera que traigo flores
-// corner cases // int vs ll // cin vs scanf // clear structures // statement // doublesz
-#include <bits/stdc++.h>
-#define pb push_back
-#define sz(x) int(x.size())
-#define fill(x,v) memset(x,v,sizeof(x))
-#define REP(i,a,b) for(int i = int(a); i < int(b); ++i)
-#define trace(x) cout << #x << " = " << x << endl
-#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> ii;
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 // for 64 bit
@@ -20,9 +8,8 @@ int main(){
   cout << rng() << endl;
   
   vector<int> perm(10);
-  iota(perm.begin(),perm.end(),0);
+  iota(perm.begin(), perm.end(), 0);
   shuffle(perm.begin(),perm.end(),rng);
-  for(int x : perm) cout << x << " "; cout << endl;
    
   // measure time
   auto cl_start = clock();
