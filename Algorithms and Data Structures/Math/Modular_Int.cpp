4fc51776791464a872082b356fa79ec20d3aa84c
Eduardo Velarde <eduardo.velarde@pucp.pe>
2022-04-05 13:46:06 -0500

    Update Modular_Int

--- Algorithms and Data Structures/Math/Modular_Int.cpp
+++ Algorithms and Data Structures/Math/Modular_Int.cpp
@@ -1,23 +1,22 @@
-// based on : https://codeforces.com/contest/1383/submission/87919031
 
 template<typename T, T mod>
-struct Tint{
+struct Modular{
   T val;
-  Tint(): val(0){}
-  Tint(long long x){ x %= mod; if(x < 0) x += mod; val = x; }
+  Modular(): val(0) {}
+  Modular(long long x) { x %= mod; if(x < 0) x += mod; val = x; }
   // basic operations
-  Tint& operator += (Tint oth){ val += oth.val; if(val >= mod) val -= mod; return *this; }
-  Tint& operator -= (Tint oth){ val -= oth.val; if(val < 0) val += mod; return *this; }
-  Tint& operator *= (Tint oth){ val = (long long)(val) * oth.val % mod; return *this; }
-  Tint operator + (Tint oth) const{ return Tint(*this) += oth; }
-  Tint operator - (Tint oth) const{ return Tint(*this) -= oth; }
-  Tint operator * (Tint oth) const{ return Tint(*this) *= oth; }
+  Modular& operator += (Modular oth) { val += oth.val; if(val >= mod) val -= mod; return *this; }
+  Modular& operator -= (Modular oth) { val -= oth.val; if(val < 0) val += mod; return *this; }
+  Modular& operator *= (Modular oth) { val = (long long)(val) * oth.val % mod; return *this; }
+  Modular operator + (Modular oth) const { return Modular(*this) += oth; }
+  Modular operator - (Modular oth) const { return Modular(*this) -= oth; }
+  Modular operator * (Modular oth) const { return Modular(*this) *= oth; }
   // if division is required
-  Tint& operator /= (Tint oth){ return *this *= oth.inverse(); }
-  Tint operator / (Tint oth) const{ return Tint(*this) /= oth; }
-  Tint inverse() const{
+  Modular& operator /= (Modular oth) { return *this *= oth.inverse(); }
+  Modular operator / (Modular oth) const { return Modular(*this) /= oth; }
+  Modular inverse() const {
     long long a = val, b = mod, u = 1, v = 0;
-    while(b){
+    while (b) {
       long long t = a/b;
       a -= t*b; swap(a,b);
       u -= t*v; swap(u,v);
@@ -26,20 +25,6 @@ struct Tint{
     return u;
   }
   // relational operators
-  bool operator < (Tint oth) const{ return val < oth.val; }
-  bool operator > (Tint oth) const{ return val > oth.val; }
-  bool operator <= (Tint oth) const{ return val <= oth.val; }
-  bool operator >= (Tint oth) const{ return val >= oth.val; }
-  bool operator == (Tint oth) const{ return val == oth.val; }
-  bool operator != (Tint oth) const{ return val != oth.val; }
+  bool operator == (Modular oth) const{ return val == oth.val; }
+  bool operator != (Modular oth) const{ return val != oth.val; }
 };
-
-// usage
-const ll MOD = 1e9+7;
-using Mint = Tint<int,MOD>;
-
-void test(){
-  Mint x;
-  x += 4;
-  cout << x.val << endl;
-}
