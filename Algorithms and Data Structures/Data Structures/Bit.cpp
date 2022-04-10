bb7fc33453a16be53773e969ce73b38943315b98
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2022-04-09 20:57:53 -0500

    Create Bit.cpp

---
+++ Algorithms and Data Structures/Data Structures/Bit.cpp
@@ -0,0 +1,31 @@
+
+template<typename T>
+struct Bit {
+  int n;
+  vector<T> bit;
+
+  Bit(int n): n(n) {
+    bit.resize(n);
+  }
+
+  void modify(int x, T v) {
+    while (x < n) {
+      bit[x] += v;
+      x = x | (x + 1);
+    }
+  }
+
+  T get(int x) {
+    T v{};
+    while (x >= 0) {
+      v += bit[x];
+      x = (x & (x + 1)) - 1;
+    }
+    return v;
+  }
+
+  T get(int l, int r) {
+    return get(r) - get(l - 1);
+  }
+};
+
