e0680f6bac60d3d047e7961d9a1b29424982eb6e
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2023-02-05 14:16:12 -0800

    Update Gauss_xor.cpp

--- Algorithms and Data Structures/Math/Gauss_xor.cpp
+++ Algorithms and Data Structures/Math/Gauss_xor.cpp
@@ -2,13 +2,12 @@
 // Based on https://codeforces.com/blog/entry/68953
 
 struct Gauss {
-  // vectors are represented by bitmasks of length d
-  int basis[d]; // basis[i] keeps the mask of the vector whose f value is i
-  int sz = 0; // current size of the basis
-
-  // insert another vector
+  // vectors are represented by bitmasks of length D (usually 30 for ai <= 1e9)
+  int basis[D]; // basis[i] keeps the mask of the vector whose f value is i
+  int sz; // current size of the basis
+ 
   void insert(int mask) {
-    for (int i = 0; i < d; ++i) {
+    for (int i = d - 1; i >= 0; --i) {
       if (((mask >> i) & 1) == 0) continue;
       if (!basis[i]) {
         basis[i] = mask;
@@ -18,7 +17,9 @@ struct Gauss {
       mask ^= basis[i];
     }
   }
-
+ 
+  // to get the maximum xor among any subset of the inserted values
+  // the order in which we iterate the bits in insert() and getmax() should be from highest to lowest bit 
   int get_max() {
     int ans = 0;
     for (int i = d - 1; i >= 0; --i) {
@@ -26,5 +27,13 @@ struct Gauss {
       ans ^= basis[i];
     }
     return ans;
+  }
+ 
+  // merge this basis with another basis
+  void operator += (Gauss g) {
+    for (int i = d - 1; i >= 0; --i) {
+      if (!g.basis[i]) continue;
+      insert(g.basis[i]);
+    }
   }
 };
