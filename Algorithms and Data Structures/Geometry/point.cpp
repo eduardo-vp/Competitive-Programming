8b5b08f6b8f27c1087c1bfcff7efe5f42aac1617
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2023-03-28 23:59:18 -0700

    Update point.cpp

--- Algorithms and Data Structures/Geometry/point.cpp
+++ Algorithms and Data Structures/Geometry/point.cpp
@@ -1,16 +1,21 @@
 
 struct Point {
-    double x, y;
-    Point() {}
-    Point(double x, double y) : x(x), y(y) {}
-    Point(const Point &p) : x(p.x), y(p.y) {}
-    Point operator + (const Point &p) const { return Point(x + p.x, y + p.y); }
-    Point operator - (const Point &p) const { return Point(x - p.x, y - p.y); }
-    Point operator * (double c) const { return Point(x * c, y * c ); }
-    Point operator / (double c) const { return Point(x / c, y / c ); }
-};
+  double x, y;
+  Point() {}
+  Point(double x, double y) : x(x), y(y) {}
+  Point(const Point &p) : x(p.x), y(p.y) {}
+  Point operator + (const Point &p) const { return Point(x + p.x, y + p.y); }
+  Point operator - (const Point &p) const { return Point(x - p.x, y - p.y); }
+  Point operator * (double c) const { return Point(x * c, y * c ); }
+  Point operator / (double c) const { return Point(x / c, y / c ); }
 
-double dot(Point p, Point q) { return p.x * q.x + p.y * q.y; }
-double norm2(Point p) { return dot(p, p); }
-double norm(Point p) { return sqrt(norm2(p)); }
-double cross(Point p, Point q) { return p.x * q.y - p.y * q.x; }
+  // for convex hull
+  bool operator < (const Point &p) const {
+    // return x < p.x || (x == p.x && y < p.y);
+    return x < p.x - EPS || (abs(x - p.x) <= EPS && y < p.y - EPS);
+  }
+  bool operator == (const Point &p) const {
+    // return x == p.x && y == p.y;
+    return abs(x - p.x) <= EPS && abs(y - p.y) <= EPS;
+  }
+};
