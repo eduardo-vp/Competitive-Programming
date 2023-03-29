a7a8ec8c073888a377bc82b4fba32a25260c5fa2
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2023-03-29 15:41:59 -0700

    Create minkowski_sum.cpp

---
+++ Algorithms and Data Structures/Geometry/minkowski_sum.cpp
@@ -0,0 +1,30 @@
+
+// Minkowski sum in O(n + m)
+// A and B should be in anti-clockwise order
+inline bool compare(Point a, Point b) {
+  // first lowest, then most to the left
+  if(a.y < b.y) return 1;
+  if(a.y == b.y) return a.x < b.x;
+  return 0;
+}
+
+vector<Point> minkow_sum(const vector<Point>& a, const vector<Point>& b) {
+  vector<Point> out;
+  int lena = int(a.size());
+  int lenb = int(b.size());
+  int i = 0, j = 0;
+  for (int q = 0; q < lena; ++q) if (compare(a[q], a[i])) i = q;
+  for (int q = 0; q < lenb; ++q) if (compare(b[q], b[j])) j = q;
+  long long pr;
+  int nxti, nxtj;
+  do {
+    out.push_back(a[i] + b[j]);
+    nxti = (i + 1) % lena;
+    nxtj = (j + 1) % lenb;
+    pr = cross(a[nxti] - a[i], b[nxtj] - b[j]);
+    if (pr > 0) i = nxti;
+    else if (pr < 0) j = nxtj;
+    else i = nxti, j = nxtj; // paralelas, subo en ambas
+  } while(!(a[i] + b[j] == out[0]));
+  return out;
+}
